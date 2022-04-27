490c68788642a47ffd6b4615ec91b1c057cbe4ac
vkt <vtiwari@radisys.com>
2022-04-27 12:45:17 +0530

    All file to git

---
+++ concat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+string1="FossLinux"
+string2="Blogsite"
+string=$string1$string2
+echo "$string is a great resource for Linux users to find relevant tutorials."
---
+++ switch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Input a number: "
+read number
+
+case $number in
+50)
+echo "Fifty!!" ;;
+100)
+echo "Double fifty!!" ;;
+*)
+echo "Neither 100 nor 200" ;;
+esac
+
+echo "New Line Added for PULL"
+echo "Adding line to create issues"
